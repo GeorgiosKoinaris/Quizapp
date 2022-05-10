3c49cb04d75d70784e675da07628bad720c37bfd
GeorgiosKoinaris <g.koinaris@gmail.com>
2022-05-10 15:20:49 +0200

    added JSON

---
+++ script.js
@@ -0,0 +1,129 @@
+let questions = [{
+        'question': 'Wie viele "große Ringe" gab es ursprünglich?',
+        'answer_1': '1',
+        'answer_2': '7',
+        'answer_3': '20',
+        'answer_4': '18',
+        'right_answer': 3
+    },
+    {
+        'question': 'In welchem Land wurde "Der Herr der Ringe" gedreht?',
+        'answer_1': 'Australien',
+        'answer_2': 'Neuseeland',
+        'answer_3': 'Neufundland',
+        'answer_4': 'Russland',
+        'right_answer': 2
+    },
+    {
+        'question': 'Wie heißt die Frau von Tom Bombadil?',
+        'answer_1': 'Silberblatt',
+        'answer_2': 'Morgenstern',
+        'answer_3': 'Goldbeere',
+        'answer_4': 'Abendstern',
+        'right_answer': 3
+    },
+    {
+        'question': 'Wie heißt das elbische Wort für "Freund"?',
+        'answer_1': 'Murloc',
+        'answer_2': 'Mellon',
+        'answer_3': 'Fergon',
+        'answer_4': 'Nelron',
+        'right_answer': 2
+    },
+    {
+        'question': 'Wo war Gondor als die Westfold fiel?',
+        'answer_1': 'Rohan',
+        'answer_2': 'Mordor',
+        'answer_3': 'Isengard',
+        'answer_4': 'Gondor',
+        'right_answer': 4
+    },
+    {
+        'question': 'Wie heißt Gollum auch noch?',
+        'answer_1': 'Smeagol',
+        'answer_2': 'Smigel',
+        'answer_3': 'Deagol',
+        'answer_4': 'Sandigmann',
+        'right_answer': 1
+    },
+    {
+        'question': 'Wie heißt der Turm in dem Saruman lebt?',
+        'answer_1': 'Isengart',
+        'answer_2': 'Orthanc',
+        'answer_3': 'Barad Dûr',
+        'answer_4': 'Argonath',
+        'right_answer': 2
+    },
+    {
+        'question': 'Wer schrieb "Der Herr der Ringe"?',
+        'answer_1': 'Robert Asprin',
+        'answer_2': 'Steven Hawking',
+        'answer_3': 'Peter Jackson',
+        'answer_4': 'J.R.R. Tolkien',
+        'right_answer': 4
+    },
+    {
+        'question': 'Wie viel spielte der erste Kinofilm "Die Gefährten" 2001 weltweit ein?',
+        'answer_1': 'ca. 857 Mio. $',
+        'answer_2': 'ca. 871 Mio. $',
+        'answer_3': 'ca. 911 Mio. $',
+        'answer_4': 'ca. 1,11 Mrd. $',
+        'right_answer': 2
+    },
+    {
+        'question': 'Wer sollte ursprünglich die Rolle der Galadriel übernehmen?',
+        'answer_1': 'Sandra Bulloc',
+        'answer_2': 'Jodie Foster',
+        'answer_3': 'Michelle Pfeiffer',
+        'answer_4': 'Lucy Lawless',
+        'right_answer': 4
+    },
+    {
+        'question': 'Wer hat keinen Elbenring?',
+        'answer_1': 'Elrond',
+        'answer_2': 'Celeborn',
+        'answer_3': 'Gandalf',
+        'answer_4': 'Galadriel',
+        'right_answer': 2
+    },
+    {
+        'question': 'Wie viel spielte der zweite Kinofilm "Die zwei Türme" 2002 weltweit ein?',
+        'answer_1': 'ca. 777 Mio. $',
+        'answer_2': 'ca. 811 Mio. $',
+        'answer_3': 'ca. 926 Mio. $',
+        'answer_4': 'ca. 1,09 Mrd. $',
+        'right_answer': 3
+    },
+    {
+        'question': 'Welches war kein Elbenring?',
+        'answer_1': 'Vilya',
+        'answer_2': 'Nenya',
+        'answer_3': 'Nomya',
+        'answer_4': 'Narya',
+        'right_answer': 3
+    },
+    {
+        'question': 'Wie viel spielte der dritte Kinofilm "Die Rückkehr des Königs" 2003 weltweit ein?',
+        'answer_1': 'ca. 757 Mio. $',
+        'answer_2': 'ca. 943 Mio. $',
+        'answer_3': 'ca. 1,01 Mrd. $',
+        'answer_4': 'ca. 1,19 Mrd. $',
+        'right_answer': 4
+    },
+    {
+        'question': 'Wie nennt Aragron seinen Sohn?',
+        'answer_1': 'Elessar',
+        'answer_2': 'Arathorn',
+        'answer_3': 'Elladan',
+        'answer_4': 'Eldarion',
+        'right_answer': 4
+    },
+    {
+        'question': 'Welchen Titel wollte Tolkien nachträglich lieber für "Die Rückkehr des Königs" verwenden?',
+        'answer_1': 'Das Ende Saurons',
+        'answer_2': 'Der Ringkrieg',
+        'answer_3': 'Die Vernichtung des Ringes',
+        'answer_4': 'Der Beginn des neuen Zeitalters',
+        'right_answer': 2
+    },
+]
