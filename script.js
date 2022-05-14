f5587e3cb67fa60413558501e5381f1363cc72a3
GeorgiosKoinaris <g.koinaris@gmail.com>
2022-05-14 11:25:23 +0200

    added functions for questions

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let questions = [{
         'answer_2': '7',
         'answer_3': '20',
         'answer_4': '18',
+        'category': 'general',
         'right_answer': 3
     },
     {
@@ -12,6 +13,7 @@ let questions = [{
         'answer_2': 'Neuseeland',
         'answer_3': 'Neufundland',
         'answer_4': 'Russland',
+        'category': 'production',
         'right_answer': 2
     },
     {
@@ -20,6 +22,7 @@ let questions = [{
         'answer_2': 'Morgenstern',
         'answer_3': 'Goldbeere',
         'answer_4': 'Abendstern',
+        'category': 'general',
         'right_answer': 3
     },
     {
@@ -28,6 +31,7 @@ let questions = [{
         'answer_2': 'Mellon',
         'answer_3': 'Fergon',
         'answer_4': 'Nelron',
+        'category': 'general',
         'right_answer': 2
     },
     {
@@ -36,6 +40,7 @@ let questions = [{
         'answer_2': 'Mordor',
         'answer_3': 'Isengard',
         'answer_4': 'Gondor',
+        'category': 'general',
         'right_answer': 4
     },
     {
@@ -44,6 +49,7 @@ let questions = [{
         'answer_2': 'Smigel',
         'answer_3': 'Deagol',
         'answer_4': 'Sandigmann',
+        'category': 'general',
         'right_answer': 1
     },
     {
@@ -52,6 +58,7 @@ let questions = [{
         'answer_2': 'Orthanc',
         'answer_3': 'Barad Dûr',
         'answer_4': 'Argonath',
+        'category': 'general',
         'right_answer': 2
     },
     {
@@ -60,6 +67,7 @@ let questions = [{
         'answer_2': 'Steven Hawking',
         'answer_3': 'Peter Jackson',
         'answer_4': 'J.R.R. Tolkien',
+        'category': 'production',
         'right_answer': 4
     },
     {
@@ -68,6 +76,7 @@ let questions = [{
         'answer_2': 'ca. 871 Mio. $',
         'answer_3': 'ca. 911 Mio. $',
         'answer_4': 'ca. 1,11 Mrd. $',
+        'category': 'production',
         'right_answer': 2
     },
     {
@@ -76,6 +85,7 @@ let questions = [{
         'answer_2': 'Jodie Foster',
         'answer_3': 'Michelle Pfeiffer',
         'answer_4': 'Lucy Lawless',
+        'category': 'production',
         'right_answer': 4
     },
     {
@@ -84,6 +94,7 @@ let questions = [{
         'answer_2': 'Celeborn',
         'answer_3': 'Gandalf',
         'answer_4': 'Galadriel',
+        'category': 'general',
         'right_answer': 2
     },
     {
@@ -92,6 +103,7 @@ let questions = [{
         'answer_2': 'ca. 811 Mio. $',
         'answer_3': 'ca. 926 Mio. $',
         'answer_4': 'ca. 1,09 Mrd. $',
+        'category': 'production',
         'right_answer': 3
     },
     {
@@ -100,6 +112,7 @@ let questions = [{
         'answer_2': 'Nenya',
         'answer_3': 'Nomya',
         'answer_4': 'Narya',
+        'category': 'general',
         'right_answer': 3
     },
     {
@@ -108,14 +121,16 @@ let questions = [{
         'answer_2': 'ca. 943 Mio. $',
         'answer_3': 'ca. 1,01 Mrd. $',
         'answer_4': 'ca. 1,19 Mrd. $',
+        'category': 'production',
         'right_answer': 4
     },
     {
-        'question': 'Wie nennt Aragron seinen Sohn?',
+        'question': 'Wie nennt Aragorn seinen Sohn?',
         'answer_1': 'Elessar',
         'answer_2': 'Arathorn',
         'answer_3': 'Elladan',
         'answer_4': 'Eldarion',
+        'category': 'general',
         'right_answer': 4
     },
     {
@@ -124,9 +139,77 @@ let questions = [{
         'answer_2': 'Der Ringkrieg',
         'answer_3': 'Die Vernichtung des Ringes',
         'answer_4': 'Der Beginn des neuen Zeitalters',
+        'category': 'production',
         'right_answer': 2
     }
 ];
 
+let currentQuestion = 0;
 
-function init() {}
+function init() {
+    document.getElementById('all-questions').innerHTML = questions.length;
+    showQuestion();
+}
+
+function showQuestion() {
+
+    if (currentQuestion >= questions.length) {
+        document.getElementById('end-screen').style = "";
+        document.getElementById('question-body').style = "display:none";
+    } else {
+
+        let question = questions[currentQuestion];
+
+
+        document.getElementById('question-number').innerHTML = currentQuestion + 1;
+        document.getElementById('questionText').innerHTML = question['question'];
+        document.getElementById('answer_1').innerHTML = question['answer_1'];
+        document.getElementById('answer_2').innerHTML = question['answer_2'];
+        document.getElementById('answer_3').innerHTML = question['answer_3'];
+        document.getElementById('answer_4').innerHTML = question['answer_4'];
+    }
+}
+
+function selectCategory() {
+
+}
+
+
+function answer(selection) {
+    let question = questions[currentQuestion];
+    let selectedAnswer = selection.slice(-1);
+
+    let idOfRightAnswer = `answer_${question['right_answer']}`;
+
+    if (selectedAnswer == question['right_answer']) {
+        document.getElementById(selection).parentNode.classList.add('bg-green');
+    } else {
+        document.getElementById(selection).parentNode.classList.add('bg-red');
+        document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-green');
+    }
+    document.getElementById('next-button').disabled = false;
+}
+
+
+
+function nextQuestion() {
+    currentQuestion++;
+    document.getElementById('next-button').disabled = true;
+    resetAnswerButtons()
+    showQuestion();
+
+
+
+}
+
+function resetAnswerButtons() {
+    document.getElementById('next-button').disabled = true;
+    document.getElementById('answer_1').parentNode.classList.remove('bg-green');
+    document.getElementById('answer_1').parentNode.classList.remove('bg-red');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-green');
+    document.getElementById('answer_2').parentNode.classList.remove('bg-red');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-green');
+    document.getElementById('answer_3').parentNode.classList.remove('bg-red');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-green');
+    document.getElementById('answer_4').parentNode.classList.remove('bg-red');
+}
